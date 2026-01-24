646566fe023881f1606bcc834b41128a42473ebd
unknown <wachirapatboonmee@gmail.com>
2026-01-24 13:10:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', `${__dirname}/public`);
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
