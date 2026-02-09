65372f000debbb339fd59926be23fa532b0dc84e
poomkrunpom <poomkrubpom@gmail.com>
2026-02-09 19:08:20 +0700

    frontend add homepage backend add value homepage  in bth.js

--- database/mysqlpool.js
+++ database/mysqlpool.js
@@ -10,4 +10,30 @@ const pool = mysql.createPool({
     port: 3306,
 });
 
-module.exports = pool
+async function total_inventory() {
+    const query = 'SELECT COUNT(*) as total FROM inventory';
+    return pool.promise().query(query);
+}
+
+async function total_money(){
+    const  query = 'SELECT SUM(remain * unit_price) AS total FROM inventory';
+    return pool.promise().query(query);
+}
+
+async function total_plan(){
+    const query = 'SELECT COUNT(*) AS total FROM plan_header';
+    return pool.promise().query(query);
+}
+
+async function total_po() {
+    const query = 'SELECT COUNT(*) AS total FROM po_header'
+    return pool.promise().query(query);
+}
+
+module.exports = {
+    pool,
+    total_inventory,
+    total_money,
+    total_plan,
+    total_po
+};
